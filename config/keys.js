d64fd19532fd883c164fc24277fbe6ba9b44b319
Fortino <contact@fortinosandoval.com>
2018-04-27 14:14:10 -0700

    Post comments and likes

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://forti:win7@ds149279.mlab.com:49279/devconnector",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
